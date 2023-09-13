aa681ac1aa680228d41601a369a6bc9c2f15576a
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2023-09-13 20:14:15 -0300

    chore(HomeFeatureCard.scss): add styles for the HomeFeatureCard component feat(HomeFeatureCard.tsx): add support for rendering different features in the HomeFeatureCard component feat(HomePage.scss): add styles for the HomePage component feat(HomePage.tsx): add HomeFeatureCard components with different features to the HomePage component

--- src/components/cards/HomeFeatureCard.tsx
+++ src/components/cards/HomeFeatureCard.tsx
@@ -1,19 +1,35 @@
 import { ReactNode } from "react";
 import "./HomeFeatureCard.scss";
 
-type Props = { children: ReactNode };
+type Props = {
+  children: ReactNode;
+  feature: "passionate" | "resourceful" | "friendly";
+};
+
+type ChildrenProps = {
+  children: ReactNode;
+};
 
 //* COMPONENT: HomeFeatureCard
-export default function HomeFeatureCard({ children }: Props) {
-  // output
-  return <article className="home-feature-card">{children}</article>;
+export default function HomeFeatureCard({ children, feature }: Props) {
+  return (
+    <article className="home-feature-card">
+      <img
+        src={`assets/home/desktop/illustration-${feature}.svg`}
+        alt={`illustration of ${feature}`}
+        className="home-feature-card__image"
+      />
+
+      <div className="home-feature-card__group">{children}</div>
+    </article>
+  );
 }
 
-function Title({ children }: Props) {
+function Title({ children }: ChildrenProps) {
   return <h3 className="home-feature-card__title">{children}</h3>;
 }
 
-function Description({ children }: Props) {
+function Description({ children }: ChildrenProps) {
   return <p className="home-feature-card__title">{children}</p>;
 }
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -30,7 +30,32 @@ export default function HomePage() {
       </section>
 
       <section className="home-page__features container-mar">
-        <HomeFeatureCard></HomeFeatureCard>
+        <HomeFeatureCard feature="passionate">
+          <HomeFeatureCard.Title>Passionate</HomeFeatureCard.Title>
+          <HomeFeatureCard.Description>
+            Each project starts with an in-depth brand research to ensure we
+            only create products that serve a purpose. We merge art, design, and
+            technology into exciting new solutions.
+          </HomeFeatureCard.Description>
+        </HomeFeatureCard>
+
+        <HomeFeatureCard feature="resourceful">
+          <HomeFeatureCard.Title>Resourceful</HomeFeatureCard.Title>
+          <HomeFeatureCard.Description>
+            Everything that we do has a strategic purpose. We use an agile
+            approach in all of our projects and value customer collaboration. It
+            guarantees superior results that fulfill our clients’ needs.
+          </HomeFeatureCard.Description>
+        </HomeFeatureCard>
+
+        <HomeFeatureCard feature="friendly">
+          <HomeFeatureCard.Title>Friendly</HomeFeatureCard.Title>
+          <HomeFeatureCard.Description>
+            We are a group of enthusiastic folks who know how to put people
+            first. Our success depends on our customers, and we strive to give
+            them the best experience a company can provide.
+          </HomeFeatureCard.Description>
+        </HomeFeatureCard>
       </section>
     </main>
   );
